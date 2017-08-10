be6c44b132998de1d45450a948b456d9bc93c810
Markus Klein <markus-klein@live.de>
2017-08-10 02:09:03 +0200

    more target implementations

--- src/target.rs
+++ src/target.rs
@@ -1,5 +1,6 @@
 use odbc_sys::*;
 use std::cmp::min;
+use std::mem::size_of;
 
 /// Indicates a type which can be used as a target in `Statement::get_data()`.
 pub unsafe trait Target {
@@ -11,7 +12,7 @@ pub unsafe trait Target {
     fn buffer_len(&self) -> SQLLEN;
 }
 
-unsafe impl Target for [u8] {
+unsafe impl Target for [SQLCHAR] {
     fn c_data_type() -> SqlCDataType {
         SQL_C_CHAR
     }
@@ -22,5 +23,155 @@ unsafe impl Target for [u8] {
 
     fn buffer_len(&self) -> SQLLEN {
         min(self.len() as SQLLEN, SQLLEN::max_value())
+    }
+}
+
+unsafe impl Target for SQLSMALLINT {
+    fn c_data_type() -> SqlCDataType {
+        SQL_C_SSHORT
+    }
+
+    fn value_ptr(&mut self) -> SQLPOINTER {
+        let ptr: *mut Self = self;
+        ptr as SQLPOINTER
+    }
+
+    fn buffer_len(&self) -> SQLLEN {
+        size_of::<Self>() as SQLLEN
+    }
+}
+
+unsafe impl Target for SQLUSMALLINT {
+    fn c_data_type() -> SqlCDataType {
+        SQL_C_USHORT
+    }
+
+    fn value_ptr(&mut self) -> SQLPOINTER {
+        let ptr: *mut Self = self;
+        ptr as SQLPOINTER
+    }
+
+    fn buffer_len(&self) -> SQLLEN {
+        size_of::<Self>() as SQLLEN
+    }
+}
+
+unsafe impl Target for SQLINTEGER {
+    fn c_data_type() -> SqlCDataType {
+        SQL_C_SLONG
+    }
+
+    fn value_ptr(&mut self) -> SQLPOINTER {
+        let ptr: *mut Self = self;
+        ptr as SQLPOINTER
+    }
+
+    fn buffer_len(&self) -> SQLLEN {
+        size_of::<Self>() as SQLLEN
+    }
+}
+
+unsafe impl Target for SQLUINTEGER {
+    fn c_data_type() -> SqlCDataType {
+        SQL_C_ULONG
+    }
+
+    fn value_ptr(&mut self) -> SQLPOINTER {
+        let ptr: *mut Self = self;
+        ptr as SQLPOINTER
+    }
+
+    fn buffer_len(&self) -> SQLLEN {
+        size_of::<Self>() as SQLLEN
+    }
+}
+
+unsafe impl Target for f32 {
+    fn c_data_type() -> SqlCDataType {
+        SQL_C_FLOAT
+    }
+
+    fn value_ptr(&mut self) -> SQLPOINTER {
+        let ptr: *mut Self = self;
+        ptr as SQLPOINTER
+    }
+
+    fn buffer_len(&self) -> SQLLEN {
+        size_of::<Self>() as SQLLEN
+    }
+}
+
+unsafe impl Target for f64 {
+    fn c_data_type() -> SqlCDataType {
+        SQL_C_DOUBLE
+    }
+
+    fn value_ptr(&mut self) -> SQLPOINTER {
+        let ptr: *mut Self = self;
+        ptr as SQLPOINTER
+    }
+
+    fn buffer_len(&self) -> SQLLEN {
+        size_of::<Self>() as SQLLEN
+    }
+}
+
+unsafe impl Target for i8 {
+    fn c_data_type() -> SqlCDataType {
+        SQL_C_STINYINT
+    }
+
+    fn value_ptr(&mut self) -> SQLPOINTER {
+        let ptr: *mut Self = self;
+        ptr as SQLPOINTER
+    }
+
+    fn buffer_len(&self) -> SQLLEN {
+        size_of::<Self>() as SQLLEN
+    }
+}
+
+unsafe impl Target for SQLCHAR {
+    fn c_data_type() -> SqlCDataType {
+        SQL_C_UTINYINT
+    }
+
+    fn value_ptr(&mut self) -> SQLPOINTER {
+        let ptr: *mut Self = self;
+        ptr as SQLPOINTER
+    }
+
+    fn buffer_len(&self) -> SQLLEN {
+        size_of::<Self>() as SQLLEN
+    }
+}
+
+unsafe impl Target for i64 {
+    fn c_data_type() -> SqlCDataType {
+        SQL_C_SBIGINT
+    }
+
+    fn value_ptr(&mut self) -> SQLPOINTER {
+        let ptr: *mut Self = self;
+        ptr as SQLPOINTER
+    }
+
+    fn buffer_len(&self) -> SQLLEN {
+        size_of::<Self>() as SQLLEN
+    }
+}
+
+unsafe impl Target for u64 {
+    fn c_data_type() -> SqlCDataType {
+        SQL_C_UBIGINT
+    }
+
+    fn value_ptr(&mut self) -> SQLPOINTER {
+        let ptr: *mut Self = self;
+        ptr as SQLPOINTER
+    }
+
+    fn buffer_len(&self) -> SQLLEN {
+        size_of::<Self>() as SQLLEN
     }
 }
