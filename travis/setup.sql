234a9645459036c3a21e52f6d6e6bd7fa5c06e86
Markus Klein <markus-klein@live.de>
2017-08-11 02:25:18 +0200

    fix quotes

--- travis/setup.sql
+++ travis/setup.sql
@@ -3,6 +3,5 @@ CREATE TABLE Movies(
     year int
 );
 
-INSERT INTO Movies (title, year) Values ("Jurassic Park", 1993);
-INSERT INTO Movies (title, year) Values ("2001: A Space Odyssey", 1968);
-COMMIT
+INSERT INTO Movies (title, year) Values ('Jurassic Park', 1993);
+INSERT INTO Movies (title, year) Values ('2001: A Space Odyssey', 1968);
