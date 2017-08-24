7c447b1ead608767d21aa2b4874480ff788149b1
Markus Klein <markus-klein@live.de>
2017-08-24 23:05:37 +0200

    Bump version to 3.1

--- src/data_source/hdbc_wrapper.rs
+++ src/data_source/hdbc_wrapper.rs
@@ -6,7 +6,10 @@ use super::*;
 /// not a `disconnect` should be executed on drop. This trait allows to handle them both in generic
 /// code and makes them syntactically very similar to a direct use of `HDbc`.
 pub trait HDbcWrapper<'env>: DerefMut<Target = HDbc<'env>> {
+    /// Type to a handle, which also must implement this trait.
     type Handle: HDbcWrapper<'env>;
+    /// Release ownership of the internal Connection Handle
     fn into_hdbc(self) -> HDbc<'env>;
+    /// Construction from a Connection Handle
     fn from_hdbc(handle: HDbc<'env>) -> Self::Handle;
 }
